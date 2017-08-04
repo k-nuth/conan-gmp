8a942663e2f2bd11da53f9154966af77779c437c
Dario Ramos <dario.fenix@gmail.com>
2017-08-04 12:40:03 -0300

    DEV-8: Add remote for publishing to Bintray

--- build.py
+++ build.py
@@ -3,19 +3,10 @@ from conan.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="bitprim", channel="stable")
+    builder = ConanMultiPackager(username="bitprim", channel="stable",
+                                 remotes="https://api.bintray.com/conan/bitprim/bitprim")
     builder.add_common_builds(shared_option_name="gmp:shared", pure_c=True)
     builder.password = os.getenv("CONAN_PASSWORD")
-
-    # if platform.system() == "Windows":  # Library not prepared to create a .lib to link with
-    #     # Remove shared builds in Windows
-    #     static_builds = []
-    #     for settings, options, env_vars, build_requires in builder.builds:
-    #         if not ("gmp:shared" in options and options["gmp:shared"]):
-    #             static_builds.append([settings, options, env_vars, build_requires])
-
-    #     builder.builds = static_builds
-
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
