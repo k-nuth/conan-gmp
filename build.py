15458326bfee94dd029d9cc4b88144255541090e
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-01-29 13:48:51 -0300

    better builds microarchitectures

--- build.py
+++ build.py
@@ -4,6 +4,17 @@ import os
 import cpuid
 import copy
 
+
+def handle_microarchs(opt_name, microarchs, filtered_builds, settings, options, env_vars, build_requires):
+    print(microarchs)
+    microarchs = list(set(microarchs))
+    print(microarchs)
+
+    for ma in microarchs:
+        opts_copy = copy.deepcopy(options)
+        opts_copy[opt_name] = ma
+        filtered_builds.append([settings, opts_copy, env_vars, build_requires])
+
 if __name__ == "__main__":
 
     # print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
@@ -23,20 +34,23 @@ if __name__ == "__main__":
                 and settings["arch"] == "x86_64" \
                 and not ("gmp:shared" in options and options["gmp:shared"]):
             
-            opt1 = copy.deepcopy(options)
-            opt2 = copy.deepcopy(options)
-            opt3 = copy.deepcopy(options)
-            opt4 = copy.deepcopy(options)
+            marchs = ["x86_64", ''.join(cpuid.cpu_microarchitecture()), "haswell", "skylake"]
+            handle_microarchs("gmp:microarchitecture", marchs, filtered_builds, settings, options, env_vars, build_requires)
+            
+            # opt1 = copy.deepcopy(options)
+            # opt2 = copy.deepcopy(options)
+            # opt3 = copy.deepcopy(options)
+            # opt4 = copy.deepcopy(options)
 
-            opt1["gmp:microarchitecture"] = "x86_64"
-            opt2["gmp:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
-            opt3["gmp:microarchitecture"] = "haswell"
-            opt4["gmp:microarchitecture"] = "skylake"
+            # opt1["gmp:microarchitecture"] = "x86_64"
+            # opt2["gmp:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
+            # opt3["gmp:microarchitecture"] = "haswell"
+            # opt4["gmp:microarchitecture"] = "skylake"
 
-            filtered_builds.append([settings, opt1, env_vars, build_requires])
-            filtered_builds.append([settings, opt2, env_vars, build_requires])
-            filtered_builds.append([settings, opt3, env_vars, build_requires])
-            filtered_builds.append([settings, opt4, env_vars, build_requires])
+            # filtered_builds.append([settings, opt1, env_vars, build_requires])
+            # filtered_builds.append([settings, opt2, env_vars, build_requires])
+            # filtered_builds.append([settings, opt3, env_vars, build_requires])
+            # filtered_builds.append([settings, opt4, env_vars, build_requires])
 
     builder.builds = filtered_builds
 
